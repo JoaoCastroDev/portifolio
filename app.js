8b1949382bc7682a3ef99bce00d96b865e360742
Joao Paulo Castro <joaocastrorn.dev@gmail.com>
2024-08-05 17:29:25 -0300

    correcao bugs botao cv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-document.getElementById('downloadCV').addEventListener('click', function() {
+document.getElementById('downloadCV').addEventListener('click', function(event) {
+    event.preventDefault();
     window.location.href = 'https://drive.google.com/uc?export=download&id=1Q3d4cfd9GQk3uLkmFRg9o-JiTmRvLRCM';
 });
