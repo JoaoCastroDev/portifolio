bc54ca630ed6cc0f56caafe321192a4cbc0ae295
Joao Paulo Castro <joaocastrorn.dev@gmail.com>
2024-08-05 17:11:16 -0300

    funcionalidade do botao cv

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 document.getElementById('downloadCV').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = 'CVJoao2024.pdf'; // Substitua pelo URL do seu arquivo PDF
-    link.download = 'CV2024.pdf'; // Nome que o arquivo terá quando baixado
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    window.location.href = 'https://drive.google.com/file/d/1Q3d4cfd9GQk3uLkmFRg9o-JiTmRvLRCM/view?usp=drive_link';
 });
