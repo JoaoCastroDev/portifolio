f8cde099cbc89ff432a7914867effaf15b82cebf
Joao Paulo Castro <joaocastrorn.dev@gmail.com>
2024-08-05 17:19:33 -0300

    funcionalidade do botao de cv

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 document.getElementById('downloadCV').addEventListener('click', function() {
-    window.location.href = 'https://drive.google.com/file/d/1Q3d4cfd9GQk3uLkmFRg9o-JiTmRvLRCM/view?usp=drive_link';
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1Q3d4cfd9GQk3uLkmFRg9o-JiTmRvLRCM';
 });
